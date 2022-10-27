34da230af4211ec8984ba6496a436a7806766ee0
Marcelearn <jaworskimarcel@gmail.com>
2022-10-27 12:30:59 +0200

    InputGroup from Bootstrap fixed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
-import InputGroup from 'react-bootstrap';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 
 export default function DateForm ({date, setDate}) {
